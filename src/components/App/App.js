16ff61e6229a544ea88e6988bea7f82027c5874e
Leah Winters <leah.winters24@gmail.com>
2020-05-18 18:58:29 -0400

    Attempt to get the correct listings from the selected area

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Areas from '../Areas/Areas.js';
 import Login from '../Login/Login.js';
+import Listings from '../Listings/Listings.js';
 import { Route, Redirect } from 'react-router-dom';
 
 class App extends Component {
@@ -11,7 +12,9 @@ class App extends Component {
       isLoggedIn: false,
       username: '',
       purpose: '',
-      areas: []
+      areas: [],
+      currentArea: 0,
+      currentListings: []
     }
   }
 
@@ -37,19 +40,24 @@ class App extends Component {
         Promise.all(areaNamePromises)
         .then(areaData => this.setState({areas: areaData}))
       })
-        .catch(err => console.error(err))
+      .catch(err => console.error(err));
+  }
+
+  getListings = (currentListings) => {
+    this.setState({...this.state, currentListings: [...currentListings]})
+    console.log(this.state.currentListings)
   }
 
   render () {
-      return (
-        <div>
-        <header className="areas-page-header">
-          <h1 className="header-text-area">
-            Vacation Rentals Around Denver
-            {this.state.isLoggedIn && <button className='sign-out-btn' type='button' onClick={this.signOut}>Sign out!</button>}
-          </h1>
-          {console.log(this.state)}
-          {!this.state.isLoggedIn && <Redirect to='/' />}
+    return (
+      <div>
+      <header className="areas-page-header">
+        <h1 className="header-text-area">
+          Vacation Rentals Around Denver
+          {this.state.isLoggedIn && <button className='sign-out-btn' type='button' onClick={this.signOut}>Sign out!</button>}
+        </h1>
+        {console.log(this.state)}
+        {!this.state.isLoggedIn && <Redirect to='/' />}
         </header>
         <main className='App'>
           <Route
@@ -69,22 +77,25 @@ class App extends Component {
             render={() => (
               <section>
                 <header>
-                    {!this.state.isLoggedIn && <Redirect to='/' />}
+                  {!this.state.isLoggedIn && <Redirect to='/' />}
                 </header>
                 <Areas
                   data={this.state.areas}
                   purpose={this.state.purpose}
                   username={this.state.username}
+                  getListings={this.getListings}
                 />
               </section>
             )}
           />
           <Route
             path='/areas/:area_id/listings'
-            render={({ location }) => (
+            render={({ location, match }) => (
               <section>
-                {console.log('listings worked!')}
-                {console.log(this.state)}
+                <Listings
+                  areaId={match.params.area_id}
+                  />
+                {console.log(match.params.area_id)}
               </section>
             )}
           />
